fe1a85373d3bed595688c1302371a8ff8ab9ab17
Rambo Sorn <rambos@scg.com>
2026-01-05 13:17:43 +0700

    Config: Allow Netlify production URL in CORS

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -5,7 +5,8 @@ export default [
       origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://dashing-longma-925d05.netlify.app"
       ],
       methods: ["GET", "POST", "PUT", "DELETE"],
       headers: ["Content-Type", "Authorization"],
